814551aa9fbe045ece943d85f3cb00fc89dfc901
Novokhatko Ivan <kentvanek80@gmail.com>
2026-08-20 17:32:13 +0300

    fix

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,9 +1,17 @@
 import axios from 'axios';
 
-const baseURL = `${process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3000'}/api`;
+const getBaseURL = () => {
+  const configuredUrl = process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, '');
+
+  if (typeof window !== 'undefined') {
+    return configuredUrl ? `${configuredUrl}/api` : '/api';
+  }
+
+  return configuredUrl ? `${configuredUrl}/api` : 'https://notehub-api.goit.study/api';
+};
 
 export const api = axios.create({
-  baseURL,
+  baseURL: getBaseURL(),
   withCredentials: true,
 });
 
